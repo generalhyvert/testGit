646f5feba620623aacf647065a2ddee615c3ccec
generalhyvert <generalhyvert@outlook.fr>
2017-04-25 16:35:26 +0200

    dessin rectangle

--- main.cpp
+++ main.cpp
@@ -1,13 +1,30 @@
+#include <string>
 #include <iostream>
 using namespace std;
 
-int main(){
-	cout << "Hello world \n j\'apprendre le c++" << endl;
-	int compteur = 3;
-	while (compteur > 0) {
-		cout << compteur << endl;
-		compteur -= 1;
+
+void dessinRectangle(int l, int h, string symbol) {
+	for (int ligne(0); ligne < h;ligne++) {
+		for (int colonne(0); colonne < l; colonne++) {
+			cout << symbol;
+		}
+		cout << endl;
 	}
+
+}
+
+
+
+int main(){
+	cout << "Hello world \n j\'apprends le c++" << endl;
+	
+	int l, h;
+	cout << "largeur du rectangle:";
+	cin >> l;
+	cout << "longueur du triangle:";
+	cin >> h;
+
+	dessinRectangle(l, h, "*");
 	system("pause");
 	return 0;
 }
