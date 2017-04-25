7e860f35cff347a9e24c29e4c532d93f561ebdfc
generalhyvert <generalhyvert@outlook.fr>
2017-04-25 16:05:19 +0200

    ajout du main en mode hello word et while

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "Hello world \n j\'apprendre le c++" << endl;
+	int compteur = 3;
+	while (compteur > 0) {
+		cout << compteur << endl;
+		compteur -= 1;
+	}
+	system("pause");
+	return 0;
+}
